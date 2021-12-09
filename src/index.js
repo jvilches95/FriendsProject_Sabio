ce1a229efea117eacd8876f9678fdc3ae3ea50ae
Gregorio Rojas <gregorio@sabio.la>
2021-12-09 15:56:54 -0800

    latest updates for BS and alerts and deps

--- src/index.js
+++ src/index.js
@@ -3,21 +3,21 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import "./services/serviceHelper";
 import "bootstrap/dist/css/bootstrap.css";
-import "react-toastify/dist/ReactToastify.css";
-import "sweetalert2/dist/sweetalert2.min.css";
-import { ToastContainer } from "react-toastify";
+import "toastr/build/toastr.min.css";
 import App from "./App";
-import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter } from "react-router-dom";
+
+import reportWebVitals from "./reportWebVitals";
+import debug from "sabio-debug";
+const _logger = debug.extend("root");
 
 ReactDOM.render(
-  <React.Fragment>
-    <App></App>
-    <ToastContainer />
-  </React.Fragment>,
+  <BrowserRouter>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </BrowserRouter>,
   document.getElementById("root")
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+reportWebVitals(_logger);
